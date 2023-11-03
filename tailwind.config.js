20d679f4facf84a011fc4e5cf8c9e3384316a617
saeedjamali89@gmail.com <saeedjamali89@gmail.com>
2023-11-03 22:39:38 +0330

    Start mobile version / Add Font / Work with Zeplin and Figma / Create Product list in mobile and desktop Version

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,14 @@ module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
-      fontFamily:{
-        sans:["iranyekan"]
-      }
+      fontFamily: {
+        sans: ["iranyekan"],
+      },
+      container: {
+        center: "center",
+        padding: "1rem",
+      },
     },
   },
   plugins: [],
-}
-
+};
