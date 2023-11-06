cfbeb33cfff56a6bc76b899ce33d8ff43a7ae109
saeedjamali89@gmail.com <saeedjamali89@gmail.com>
2023-11-06 12:15:29 +0330

    Add SlideBar with Js

--- src/js/app.js
+++ src/js/app.js
@@ -69,9 +69,140 @@ let str = `    <div class="bg-slate-50 rounded-md p-2">
 </div>
 </div>`;
 
-let result= "";
-for(let i=0;i<10;i++){
-    result += str;
+let result = "";
+for (let i = 0; i < 10; i++) {
+  result += str;
 }
 
 products.innerHTML = result;
+
+const sidebarFilter = document.querySelector(".sidebar__filter");
+const arrBrand = ["اپل", "سامسونگ", "هوآوی", "الجی"];
+let svgBrand = `<svg
+xmlns="http://www.w3.org/2000/svg"
+viewBox="0 0 24 24"
+fill="currentColor"
+class="w-5 h-5"
+>
+<path
+  fill-rule="evenodd"
+  d="M12 1.5a.75.75 0 01.75.75V4.5a.75.75 0 01-1.5 0V2.25A.75.75 0 0112 1.5zM5.636 4.136a.75.75 0 011.06 0l1.592 1.591a.75.75 0 01-1.061 1.06l-1.591-1.59a.75.75 0 010-1.061zm12.728 0a.75.75 0 010 1.06l-1.591 1.592a.75.75 0 01-1.06-1.061l1.59-1.591a.75.75 0 011.061 0zm-6.816 4.496a.75.75 0 01.82.311l5.228 7.917a.75.75 0 01-.777 1.148l-2.097-.43 1.045 3.9a.75.75 0 01-1.45.388l-1.044-3.899-1.601 1.42a.75.75 0 01-1.247-.606l.569-9.47a.75.75 0 01.554-.68zM3 10.5a.75.75 0 01.75-.75H6a.75.75 0 010 1.5H3.75A.75.75 0 013 10.5zm14.25 0a.75.75 0 01.75-.75h2.25a.75.75 0 010 1.5H18a.75.75 0 01-.75-.75zm-8.962 3.712a.75.75 0 010 1.061l-1.591 1.591a.75.75 0 11-1.061-1.06l1.591-1.592a.75.75 0 011.06 0z"
+  clip-rule="evenodd"
+/>
+</svg>`;
+
+const arrPrice = [
+  "کمتر از ده میلیون",
+  "کمتر از صد میلیون",
+  "کمتر از میلیارد",
+  "سایر",
+];
+let svgPrice = `   <svg
+xmlns="http://www.w3.org/2000/svg"
+fill="none"
+viewBox="0 0 24 24"
+stroke-width="1.5"
+stroke="currentColor"
+class="w-5 h-5"
+>
+<path
+  stroke-linecap="round"
+  stroke-linejoin="round"
+  d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5z"
+/>
+</svg>
+`;
+
+createCheckedGroup("برند محصول", svgBrand, arrBrand, 1);
+createCheckedGroup("قیمت محصول", svgPrice, arrPrice, 2);
+createCheckedGroup("برند محصول", svgBrand, arrBrand, 3);
+createCheckedGroup("قیمت محصول", svgPrice, arrPrice, 4);
+
+function createCheckedGroup(title, icon, arr, id) {
+  let items = "";
+
+  arr.forEach((element) => {
+    items += `
+    <li class="w-full rounded-lg mr-2">
+    <div class="flex items-center pl-3">
+      <input
+        id="vue-checkbox"
+        type="checkbox"
+        value=""
+        class="w-4 h-4 ring-orange-500 bg-purple-800"
+      />
+      <label
+        for="vue-checkbox"
+        class="w-full py-3 mr-2 text-sm font-medium text-gray-900"
+        >${element}
+      </label>
+    </div>
+  </li>`;
+  });
+
+  let checkBoxGroup = ` <div class=" cursor-pointer w-full mt-4 bg-slate-200 p-2 rounded-md">
+<div data-id=${id}  class="title__filter w-full flex items-center justify-between">
+  <div class="flex">
+    <span>
+     ${icon}
+    </span>
+    <span class="mr-2 font-medium"> ${title}</span>
+  </div>
+  <div>
+    <span>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="w-5 h-5"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+        />
+      </svg>
+    </span>
+  </div>
+</div>
+<div data-id=${id} class="content__filter mt-2 z-20 ">
+  <ul
+    class="w-full text-sm font-medium text-gray-900 bg-white rounded-md"
+  >
+  ${items}    
+  </ul>
+</div>
+</div>
+`;
+
+  sidebarFilter.innerHTML += checkBoxGroup;
+  disClosureSideBar();
+}
+
+function disClosureSideBar() {
+  const titles = document.querySelectorAll(".title__filter");
+  const content = document.querySelectorAll(".content__filter");
+  // console.log(titles);
+
+  titles.forEach((title) => {
+    title.addEventListener("click", (e) => {
+      // console.log(title.childNodes[3].childNodes[1].classList);
+      title.childNodes[3].childNodes[1].classList.toggle("rotate-180");
+      let id = title.dataset.id;
+      content.forEach((c) => {
+        if (c.dataset.id == id) {
+          c.classList.toggle("hidden");
+        } else {
+          // title.childNodes[3].childNodes[1].classList.toggle("rotate-180");
+          c.parentNode
+            .getElementsByTagName("div")[0]
+            .childNodes[3].childNodes[1].classList.remove("rotate-180");
+          c.classList.add("hidden");
+        }
+        c.addEventListener("click", () => console.log("Next time..."));
+      });
+    });
+  });
+}
